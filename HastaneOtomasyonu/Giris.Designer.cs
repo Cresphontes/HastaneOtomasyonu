26993628ac15347780fbe2235de1e16504403e25
Bugra Kırman <bugrakirman@gmail.com>
2018-12-12 09:56:44 +0300

    Giriş formu düzenlendi

--- HastaneOtomasyonu/Giris.Designer.cs
+++ HastaneOtomasyonu/Giris.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(78, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 106);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Randevu Al";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(230, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 106);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Hasta Kayıt";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(382, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 106);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Doktor Kayıt";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(78, 209);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 106);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Hemşire Kayıt";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(230, 209);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 106);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Personel Kayıt";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // FormGiris
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(573, 382);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "FormGiris";
             this.Text = "Giris";
             this.ResumeLayout(false);
@@ -42,6 +102,12 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
