120a885d99f8436f9ffa5f940d5e194482d6cfd2
Bugra Kırman <bugrakirman@gmail.com>
2018-12-12 11:50:33 +0300

    maas string yapıldı

---
+++ HastaneOtomasyonu/ClassLib/Hemsire.cs
@@ -0,0 +1,14 @@
+﻿using HastaneOtomasyonu.Class_Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HastaneOtomasyonu.ClassLib
+{
+    public class Hemsire : Kisi, IMaasAlabilir
+    {
+        public string Maas { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
