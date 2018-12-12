40e2e154f137993203dd84c67648196b1275a993
Bugra Kırman <bugrakirman@gmail.com>
2018-12-12 11:04:05 +0300

    Enumlar eklendi

---
+++ HastaneOtomasyonu/DoktorBranslari.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HastaneOtomasyonu
+{
+    public enum DoktorBranslari
+    {
+        GenelCerrahi,
+        IcHastaliklari,
+        Ortopedi,
+        Uroloji,
+        KBB,
+        CocukSagligi,
+        Kardiyoloji,
+        GözHastaliklaari
+    }
+}
