5bcb0fce7325d85be8963dc11f9147a1fde7e82a
İsmail Çadır <ismailcadir@gmail.com>
2018-12-14 13:47:58 +0300

    Randevu Bilgileri için Randevular Clası Eklendi

---
+++ HastaneOtomasyonu/ClassLib/Randevular.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HastaneOtomasyonu.ClassLib
+{
+  public  class Randevular
+    {
+        public Doktor RandevuDoktor { get; set; }
+        public Hasta RandevuHasta { get; set; }
+        public DoktorBranslari RandevuBrans { get; set; }
+        public string RandevuSaat { get; set; }
+        
+    }
+}
