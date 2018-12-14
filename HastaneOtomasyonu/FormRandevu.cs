ab67257774fe8807d96721a9fb1717ea94c14486
İsmail Çadır <ismailcadir@gmail.com>
2018-12-14 11:28:02 +0300

    Hasta Randevu Saatini alma ve butonların oluşumu eklendi.

--- HastaneOtomasyonu/FormRandevu.cs
+++ HastaneOtomasyonu/FormRandevu.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,39 +14,37 @@ namespace HastaneOtomasyonu
 {
     public partial class FormRandevu : Form
     {
-        Button btn = new Button();
+      
+
         public FormRandevu()
         {
             InitializeComponent();
             this.Load += FormRandevu_Load;
 
-            btn.MouseClick += Btn_MouseClick;
-
-
         }
 
-        private void Btn_MouseClick(object sender, MouseEventArgs e)
+        Hasta h = new Hasta();
+        MyButton b;
+        public class MyButton : Button
         {
-
+            protected override Size DefaultSize => Size = new Size(65, 60);
+            public override Color BackColor { get => Color.Peru; }
 
         }
-
         private void FormRandevu_Load(object sender, EventArgs e)
         {
             //Kayıtlı hastalar combobox'a eklendi.
             cmbHastaSec.Items.AddRange((this.MdiParent as FormGiris).hastalar.ToArray());
             //checkedListBox1.Items.AddRange((this.MdiParent as FormGiris).hastalar.ToArray());
-            
-        
-
-            lblServisSec.Visible = false;
-            cmbServisSec.Visible = false;
-            lblDoktorSec.Visible = false;
-            cmbDoktorSec.Visible = false;
-            flwRandevu.Visible = false;
-            btnRandevuKaydet.Visible = false;
+            //lblServisSec.Visible = false;
+            //cmbServisSec.Visible = false;
+            //lblDoktorSec.Visible = false;
+            //cmbDoktorSec.Visible = false;
+            //flwRandevu.Visible = false;
+            //btnRandevuKaydet.Visible = false;
 
             //Butonların üstlerine saatleri yazıyoruz.
+
             for (int saat = 9; saat <= 16; saat++)
             {
 
@@ -55,18 +54,83 @@ namespace HastaneOtomasyonu
                 }
                 for (int dakika = 0; dakika <= 45; dakika += 15)
                 {
-                    int i = 0;
-                    Button btn = new Button();
-                    //Button btn = (Button)sender;
-                    btn.BackColor = Color.Peru;
-                    btn.Size = new Size(65, 60);
-                    btn.Text = saat.ToString("00") + ":" + dakika.ToString("00");
-                    btn.Name = i.ToString();
-                    flwRandevu.Controls.Add(btn);
 
-                }
+                    b = new MyButton();
+                    b.Text = saat.ToString("00") + ":" + dakika.ToString("00");
+                    flwRandevu.Controls.Add(b);
+                    b.Click += B_Click;
+             }
 
             }
+
+
+
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+
+            h.RandevuSaati = (sender as MyButton).Text;
+           
+            //Her Gorgune inanma-- MesutHocaya İnan :)
+           // if ((sender as MyButton).Text == "09:00")
+           //     h.RandevuSaati = "09:00";
+           // else if ((sender as MyButton).Text == "09:15")
+           //     h.RandevuSaati = "09:15";
+           //else  if ((sender as MyButton).Text == "09:30")
+           //     h.RandevuSaati = "09:30";
+           // else if ((sender as MyButton).Text == "09:45")
+           //     h.RandevuSaati = "09:45";
+           // else if ((sender as MyButton).Text == "10:00")
+           //     h.RandevuSaati = "10:00";
+           // else if ((sender as MyButton).Text == "10:15")
+           //     h.RandevuSaati = "10:15";
+           // else if ((sender as MyButton).Text == "10:30")
+           //     h.RandevuSaati = "10:30";
+           // else if ((sender as MyButton).Text == "10:45")
+           //     h.RandevuSaati = "10:45";
+           // else if ((sender as MyButton).Text == "11:00")
+           //     h.RandevuSaati = "11:00";
+           // else if ((sender as MyButton).Text == "11:15")
+           //     h.RandevuSaati = "11:15";
+           // else if ((sender as MyButton).Text == "11:30")
+           //     h.RandevuSaati = "11:30";
+           // else if ((sender as MyButton).Text == "11:45")
+           //     h.RandevuSaati = "11:45";
+           // else if ((sender as MyButton).Text == "13:00")
+           //     h.RandevuSaati = "13:00";
+           // else if ((sender as MyButton).Text == "13:15")
+           //     h.RandevuSaati = "13:15";
+           // else if ((sender as MyButton).Text == "13:30")
+           //     h.RandevuSaati = "13:30";
+           // else if ((sender as MyButton).Text == "13:45")
+           //     h.RandevuSaati = "13:45";
+           // else if ((sender as MyButton).Text == "14:00")
+           //     h.RandevuSaati = "14:00";
+           // else if ((sender as MyButton).Text == "14:15")
+           //     h.RandevuSaati = "14:15";
+           // else if ((sender as MyButton).Text == "14:30")
+           //     h.RandevuSaati = "14:30";
+           // else if ((sender as MyButton).Text == "14:45")
+           //     h.RandevuSaati = "14:45";
+           // else if ((sender as MyButton).Text == "15:00")
+           //     h.RandevuSaati = "15:00";
+           // else if ((sender as MyButton).Text == "15:15")
+           //     h.RandevuSaati = "15:15";
+           // else if ((sender as MyButton).Text == "15:30")
+           //     h.RandevuSaati = "15:30";
+           // else if ((sender as MyButton).Text == "15:45")
+           //     h.RandevuSaati = "15:45";
+           // else if ((sender as MyButton).Text == "15:00")
+           //     h.RandevuSaati = "16:00";
+           // else if ((sender as MyButton).Text == "16:15")
+           //     h.RandevuSaati = "16:15";
+           // else if ((sender as MyButton).Text == "16:30")
+           //     h.RandevuSaati = "16:30";
+           // else if ((sender as MyButton).Text == "16:45")
+           //     h.RandevuSaati = "16:45";
+
+
 
 
 
@@ -81,7 +145,7 @@ namespace HastaneOtomasyonu
 
         private void cmbServisSec_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ////Kayıtlı doktorlar Alındı.
+            //Kayıtlı doktorlar Alındı.
             cmbDoktorSec.Items.AddRange((this.MdiParent as FormGiris).doktorlar.ToArray());
             lblDoktorSec.Visible = true;
             cmbDoktorSec.Visible = true;
